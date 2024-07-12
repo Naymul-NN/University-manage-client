605b570d0de0298aa1149fa88706d959c3cb0514
Naymul-NN <naymulislam108@gmail.com>
2024-07-12 12:00:35 +0600

    add login form and set the refress token into the cookies

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,39 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
+interface LoginFormInputs {
+     id: string;
+     password: string;
+   }
 
 const Login = () => { 
+     const {register,handleSubmit} = useForm<LoginFormInputs>();
+
+      const [login , {data , error}] = useLoginMutation()
+      console.log(data);
+      console.log(error);
+
+
+     const onSubmit = (data:LoginFormInputs) => {
+          const userInfo = {
+               id: data.id,
+               password: data.password,
+          }
+          login(userInfo)
+     }
 return ( 
-     <div>
-          <h1> This is Login page .. </h1>
-     </div>
+     <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+               <label htmlFor="id"> id:</label>
+               <input type=" text" id='id' {...register('id')}/>
+          </div>
+          <div>
+               <label htmlFor="password"> password</label>
+               <input type="text" id='password' {...register('password')} />
+          </div>
+          <Button htmlType="submit">Login</Button>
+     </form>
 );
 };
 
